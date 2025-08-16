f103f2dd50fb9a3219217926a6d6087e083fa7bc
Jjoon0513 <ijjun244@gmail.com>
2025-08-16 22:54:41 +0900

    Add initial Motor struct and setup for Arduino
    Introduces a generic Motor struct with IN1, IN2, and PWM pins, and a MotorStatus enum for direction control. Adds static instances for two motors and begins setup function with Arduino HAL peripherals and PWM timer initialization.

--- src/util/moter.rs
+++ src/util/moter.rs
@@ -1,9 +1,31 @@
+use arduino_hal::{Peripherals, Pins};
+use arduino_hal::port::mode::*;
+use arduino_hal::port::*;
+use arduino_hal::simple_pwm::Prescaler::Prescale64;
+use arduino_hal::simple_pwm::{IntoPwmPin, Timer2Pwm};
 
-struct Moter {
 
+//PinInit
+
+struct Motor<IN1, IN2, PWM> {
+    IN1: Pin<Output, IN1>,
+    IN2: Pin<Output, IN2>,
+    PWM: Pin<PwmOutput<Timer2Pwm>, PWM>
 }
+
+enum MotorStatus {
+    Forward(u8), 
+    Backward(u8),
+    Stop,
+}
+static mut MOTOR1: Option<Motor<D22, D23, D9>> = None;
+static mut MOTOR2: Option<Motor<D24, D25, D10>> = None;
+
 pub fn setup(){
 
+    let dp:Peripherals = Peripherals::take().unwrap();
+    let pins: Pins = arduino_hal::pins!(dp);
+    let mut timer: Timer2Pwm = Timer2Pwm::new(dp.TC2, Prescale64);
 }
 
 pub fn _loop(){
